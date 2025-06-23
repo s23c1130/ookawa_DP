52b244a5a8e6a2343453d260fb27ff06dc545433
23C1130 <s23c1130sm@s.chibakoudai.jp>
2025-06-23 19:15:06 +0900

    整合窓タイプのプログラムを追加

---
+++ ookawa_dpmatching_window.c
@@ -0,0 +1,210 @@
+//  SPDX-FileCopyrightText: 2025 Toki Makabe <s23c1130sm@s.chibakoudai.jp>
+//  SPDX-License-Identifier:BSD-3-Clause
+
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define INPUT_FILE "city_mcepdata/city011/city011_"
+#define DATASET_FILE "city_mcepdata/city012/city012_"
+#define PICTURE_PATH "pic1.ppm"
+#define DATASET_NUM_MAX 100
+#define DATASET_FRAME_MAX 200
+#define DATASET_DIMENSION_MAX 15
+#define WINDOW 50
+
+#define TATE_YOKO_COST 1
+#define NANAME_COST 2
+
+//経過時間測定関係
+clock_t time_start;
+clock_t time_end;
+
+// 局所距離を求める関数
+double d_calc(int dataset_num, int input_num, int x, int y);
+
+double dataset[DATASET_NUM_MAX][DATASET_FRAME_MAX][DATASET_DIMENSION_MAX];
+double input_data[DATASET_NUM_MAX][DATASET_FRAME_MAX][DATASET_DIMENSION_MAX];
+int dataset_frame[DATASET_NUM_MAX];
+int input_frame[DATASET_NUM_MAX];
+
+int main(void)
+{
+    //処理開始
+    time_start = clock();
+
+    unsigned char dumy[1000];
+    unsigned char file_name[1000];
+    // テンプレートのファイルを開く
+
+    // データセットの取り込み
+    for (int i = 0; i < DATASET_NUM_MAX; i++)
+    {
+        int frame;
+        snprintf(file_name, sizeof(file_name), "%s%03d.txt", DATASET_FILE, i + 1);
+        FILE *fp;
+        fp = fopen(file_name, "r");
+        if (fp == NULL)
+        {
+            printf("%s\n開けませんでした\n", file_name);
+            break;
+        }
+
+        // 上2つのいらないデータをdumyに流す
+        fscanf(fp, "%s", dumy);
+        fscanf(fp, "%s", dumy);
+        fscanf(fp, "%d", &frame);
+
+        dataset_frame[i] = frame;
+
+        for (int frame_cnt = 0; frame_cnt < frame; frame_cnt++)
+        {
+            for (int dim_cnt = 0; dim_cnt < DATASET_DIMENSION_MAX; dim_cnt++)
+            {
+                fscanf(fp, "%lf", &dataset[i][frame_cnt][dim_cnt]);
+            }
+        }
+    }
+
+    // 入力データの読み込み
+    for (int i = 0; i < DATASET_NUM_MAX; i++)
+    {
+        snprintf(file_name, sizeof(file_name), "%s%03d.txt", INPUT_FILE, i + 1);
+        FILE *input_fp;
+        input_fp = fopen(file_name, "r");
+
+        if (input_fp == NULL)
+        {
+            printf("INPUTデータが開けませんでした\n");
+            return -1;
+        }
+
+        int frame;
+
+        // 上2つのいらないデータをdumyに流す
+        fscanf(input_fp, "%s", dumy);
+        fscanf(input_fp, "%s", dumy);
+        fscanf(input_fp, "%d", &frame);
+
+        input_frame[i] = frame;
+
+        for (int frame_cnt = 0; frame_cnt < input_frame[i]; frame_cnt++)
+        {
+            for (int dim_cnt = 0; dim_cnt < DATASET_DIMENSION_MAX; dim_cnt++)
+            {
+                fscanf(input_fp, "%lf", &input_data[i][frame_cnt][dim_cnt]);
+            }
+        }
+        fclose(input_fp);
+    }
+
+    //正解の個数を入れておく変数
+    int correct_cnt = 0; //正解
+    int incorrect_cnt = 0; //不正解
+
+    for (int input_data_cnt = 0; input_data_cnt < DATASET_NUM_MAX; input_data_cnt++)
+    {
+        printf("\n\n%d 処理開始...\n", input_data_cnt + 1);
+        double min_data = __DBL_MAX__;
+        int min_asc;
+
+        // DPマッチング
+        for (int i = 0; i < DATASET_NUM_MAX; i++)
+        {
+
+            double dp[dataset_frame[i]][input_frame[input_data_cnt]];
+
+            int dataset_cnt_start = (-1) * WINDOW - 1;
+            int dataset_cnt_end = WINDOW - 1;
+
+            for (int input_cnt = 0; input_cnt < input_frame[input_data_cnt]; input_cnt++)
+            {
+                dataset_cnt_start++;
+                dataset_cnt_end++;
+
+                int dataset_cnt_start_tmp = dataset_cnt_start;
+
+                if(dataset_cnt_start_tmp < 0){
+                    dataset_cnt_start_tmp = 0;
+                }
+
+                for (int dataset_cnt = dataset_cnt_start_tmp; dataset_cnt < dataset_frame[i]; dataset_cnt++)
+                {
+                    if(dataset_cnt >= dataset_cnt_end){
+                        break;
+                    }
+
+                    double min_cost = __DBL_MAX__;
+
+                    if (input_cnt == 0 && dataset_cnt == 0)
+                    {
+                        min_cost = d_calc(i, input_data_cnt, input_cnt, dataset_cnt);
+                    }
+
+                    if (input_cnt >= 1 && dataset_cnt < dataset_cnt_end - 1)
+                    {
+                        double tmp = dp[dataset_cnt][input_cnt - 1] + d_calc(i, input_data_cnt, input_cnt, dataset_cnt) * TATE_YOKO_COST;
+                        if (min_cost >= tmp)
+                        {
+                            min_cost = tmp;
+                        }
+                    }
+                    if (dataset_cnt >= 1 && dataset_cnt > dataset_cnt_start)
+                    {
+                        double tmp = dp[dataset_cnt - 1][input_cnt] + d_calc(i, input_data_cnt, input_cnt, dataset_cnt) * TATE_YOKO_COST;
+                        if (min_cost >= tmp)
+                        {
+                            min_cost = tmp;
+                        }
+                    }
+                    if (input_cnt >= 1 && dataset_cnt >= 1)
+                    {
+                        double tmp = dp[dataset_cnt - 1][input_cnt - 1] + d_calc(i, input_data_cnt, input_cnt, dataset_cnt) * NANAME_COST;
+                        if (min_cost >= tmp)
+                        {
+                            min_cost = tmp;
+                        }
+                    }
+                    dp[dataset_cnt][input_cnt] = min_cost;
+                }
+            }
+
+            if (min_data > dp[dataset_frame[i] - 1][input_frame[input_data_cnt] - 1])
+            {
+                min_data = dp[dataset_frame[i] - 1][input_frame[input_data_cnt] - 1];
+                printf("%d %lf\n", i + 1, min_data);
+                min_asc = i;
+            }
+        }
+
+        if(input_data_cnt == min_asc){
+            printf("正解！\n");
+            correct_cnt++;
+        }
+        else{
+            printf("不正解 ＿|￣|○\n");
+            incorrect_cnt++;
+        }
+    }
+
+    printf("\n\n総数：%d 正解数：%d 不正解数:%d\n", correct_cnt + incorrect_cnt, correct_cnt, incorrect_cnt);
+    printf("正解率：%lf %%\n", (float)correct_cnt / ((float)correct_cnt + (float)incorrect_cnt) * 100);
+
+    //処理終了
+    time_end = clock();
+    printf("処理時間:%f [s]\n", ((double)time_end - (double)time_start) / CLOCKS_PER_SEC);
+
+}
+
+// 局所距離を求める関数
+// dataset_num: 使うデータセット x: 横軸 y;縦軸
+double d_calc(int dataset_num, int input_num, int x, int y)
+{
+    double sum = 0;
+    for (int i = 0; i < DATASET_DIMENSION_MAX; i++)
+    {
+        sum += pow(dataset[dataset_num][x][i] - input_data[input_num][y][i], 2);
+    }
+    return sqrt(sum);
+}
